87c82c5cd24e4844dcbe96c7c508a7e149c231a2
Mark Artishuk <markyshuk@gmail.com>
2019-11-05 09:22:16 -0800

    Added links to facebook/youtube

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from "react";
 import "./App.scss";
-import NavBar from "./components/NavBar/NavBar";
-import Header from "./components/Header/Header";
 import IntroBody from "./components/IntroBody/IntroBody";
 import FullLanding from "./components/FullLanding/FullLanding";
 
 function App() {
   return (
     <div className="App">
-      {/* <NavBar />
-      <Header /> */}
       <FullLanding />
       <IntroBody />
     </div>
--- src/components/MediaCard/MediaCard.jsx
+++ src/components/MediaCard/MediaCard.jsx
@@ -6,16 +6,22 @@ const MediaCard = () => {
     <div className="info-card shadow">
       <h2>Get Connected</h2>
       <div className="media">
-        <i class="fab fa-youtube fa-2x youtube"></i>
-        <p>Youtube</p>
+        <a href="https://www.youtube.com/channel/UCNaKPci4jHkzFyo2hYwlVRQ">
+          <i class="fab fa-youtube fa-2x youtube"></i>
+          <p>Youtube</p>
+        </a>
       </div>
       <div className="media">
-        <i class="fab fa-facebook-f fa-2x facebook"></i>
-        <p>Facebook</p>
+        <a href="https://www.facebook.com/GraceTrinityChurch/?ref=nf&hc_ref=ART03pEjMPC3cd-tbTp_iAhT6IPlHWrCdI2wgyh_HWggVSb27aafVQL5ELahZNU9vZY">
+          <i class="fab fa-facebook-f fa-2x facebook"></i>
+          <p>Facebook</p>
+        </a>
       </div>
       <div className="media">
-        <i class="fab fa-instagram fa-2x instagram"></i>
-        <p>Instagram</p>
+        <a href="">
+          <i class="fab fa-instagram fa-2x instagram"></i>
+          <p>Instagram</p>
+        </a>
       </div>
     </div>
   );
