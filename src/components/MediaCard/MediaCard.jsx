a9e57ee3a58adfc80b06e1dd583f5d0b83fbb480
Mark Artishuk <markyshuk@gmail.com>
2019-11-04 17:31:00 -0800

    Added more options to media component

--- src/components/MediaCard/MediaCard.jsx
+++ src/components/MediaCard/MediaCard.jsx
@@ -5,7 +5,18 @@ const MediaCard = () => {
   return (
     <div className="info-card">
       <h2>Get Connected</h2>
-      <div className="media"></div>
+      <div className="media">
+        <i class="fab fa-youtube fa-2x youtube"></i>
+        <p>Youtube</p>
+      </div>
+      <div className="media">
+        <i class="fab fa-facebook-f fa-2x facebook"></i>
+        <p>Facebook</p>
+      </div>
+      <div className="media">
+        <i class="fab fa-instagram fa-2x instagram"></i>
+        <p>Instagram</p>
+      </div>
     </div>
   );
 };
